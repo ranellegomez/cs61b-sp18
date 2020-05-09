32beb0387a1fcb6ab12bc09f5295bffdfe8ea038
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-05-08 17:33:05 -0700

    finished DebugEx2

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -1,3 +1,4 @@
+import java.util.stream.IntStream;
 /**
  * Exercise to showcase the step over button.
  * Code adapted from https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
@@ -7,13 +8,7 @@ public class DebugExercise2 {
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        return Math.max(a, b);
     }
 
 
@@ -57,13 +52,7 @@ public class DebugExercise2 {
 
     /** Returns the sum of all elements in x. */
     public static int arraySum(int[] x) {
-        int i = 0;
-        int sum = 0;
-        while (i < x.length) {
-            sum = sum + add(sum, x[i]);
-            i = i + 1;
-        }
-        return sum;
+        return IntStream.of(x).sum();
     }
 
     /** Returns the sum of the element-wise max of a and b.
