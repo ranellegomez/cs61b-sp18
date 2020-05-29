6a17a61c69c28317249dd90ddd6dd43675b24585
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-05-29 00:26:23 -0700

    bs not working

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -2,14 +2,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-=======
->>>>>>> parent of 9aabdc5... og readPlanets
 
 public class NBody {
 
@@ -22,7 +19,6 @@ public class NBody {
             return 0;
         }
     }
-<<<<<<< HEAD
 
     /** Given a String FILEPATH, returns an array of planets inside the file.
      * public Planet(double xP, double yP, double xV, double yV, double m,
@@ -31,47 +27,46 @@ public class NBody {
         try {
             Planet[] planetArray;
             int lineNumber = 2;
+            int planetCount = 0;
             String fileLine = Files.readAllLines(Paths.get(filePath)).get(lineNumber);
-            planetArray = new Planet[5];
-            HashMap<String, Planet> planetHashMap = new HashMap<String, Planet>();
-            HashSet<Planet> planetSet = new HashSet<>();
+            HashMap<String, Planet> planetHashMap = new HashMap<>();
 
             while (fileLine != null && isNumeric((fileLine.trim().split(
                     "\\s+")[0]))) {
 
                 fileLine = fileLine.trim();
-                System.out.println(fileLine);
                 String[] planetInfo = fileLine.split("\\s+");
-                System.out.println(Arrays.toString(planetInfo));
                 Planet p = new Planet(Double.parseDouble(planetInfo[0]),
                                       Double.parseDouble(planetInfo[1]),
                                       Double.parseDouble(planetInfo[2]),
                                       Double.parseDouble(planetInfo[3]),
                                       Double.parseDouble(planetInfo[4]),
                                       planetInfo[5]);
-                if (!planetSet.contains(p)) {
-                    planetSet.add(p);
-                }
-                for (Planet pep: planetSet) {
-                    System.out.println(pep.imgFileName);
-
-                    // need to debug this. Puts venus 5x.
-                    /**
-                    venus.gif
-                    venus.gif
-                    venus.gif
-                    venus.gif
-                    venus.gif
-                     */
-
-                }
+                planetHashMap.put(planetInfo[5], p);
                 fileLine =
                         Files.readAllLines(Paths.get(filePath)).get(lineNumber++);
+                planetCount++;
+            }
+            lineNumber = 2;
+            fileLine = Files.readAllLines(Paths.get(filePath)).get(lineNumber);
+            planetArray = new Planet[5];
+            for (int j = 0; j < 5; j++) {
+                fileLine = fileLine.trim();
+                String[] planetInfo = fileLine.split("\\s+");
+                Planet p = new Planet(Double.parseDouble(planetInfo[0]),
+                                      Double.parseDouble(planetInfo[1]),
+                                      Double.parseDouble(planetInfo[2]),
+                                      Double.parseDouble(planetInfo[3]),
+                                      Double.parseDouble(planetInfo[4]),
+                                      planetInfo[5]);
+                planetArray[j] = p;
+                fileLine = Files.readAllLines(Paths.get(filePath)).get(lineNumber++);
             }
             int i = 0;
             for (Planet p: planetHashMap.values()) {
                 planetArray[i] = p;
                 i++;
+
             }
             return planetArray;
         } catch (IOException e) {
@@ -119,3 +114,4 @@ public class NBody {
         StdDraw.show();
     }
 }
+
