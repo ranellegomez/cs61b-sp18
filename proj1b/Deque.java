36a5749829b0192898314e29a8fe83f35d15f7ab
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-09-11 18:48:04 -0700

    added the interface

---
+++ proj1b/Deque.java
@@ -0,0 +1,35 @@
+public interface Deque<T> {
+
+    /** Adds an item to the front of the deque. */
+    void addFirst(T item);
+
+    /** Adds an item to the back of the deque. */
+    void addLast(T item);
+
+    /** Returns true if empty; false otherwise. */
+    void isEmpty(T item);
+
+
+    /** Returns the number of items in this deque. */
+    int size();
+
+
+    /** Prints each item of this deque line-by-line starting with the first
+     * element.
+     * */
+    void printDeque();
+
+
+    /** Removes and returns the first item of this deque. */
+    T removeFirst();
+
+
+    /** Removes and returns the last item of this deque. */
+    T removeLast();
+
+
+    /** Returns the ith item of this deque. */
+    T get(int i);
+
+
+}
