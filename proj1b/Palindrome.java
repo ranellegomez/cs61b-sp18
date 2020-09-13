32dcbc4863269695a1e363a64eeab1b83b9957f0
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-09-12 21:11:42 -0700

    Got my Deque to run

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -6,7 +6,7 @@ public class Palindrome {
     public Deque<Character> wordToDeque(String word) {
         Deque<Character> result = new ArrayDeque<>();
         for (int i = 0; i < word.length(); i++) {
-            result.addFirst(word.charAt(i));
+            result.addLast(word.charAt(i));
         }
         return result;
     }
