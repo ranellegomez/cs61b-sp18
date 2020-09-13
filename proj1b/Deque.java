d2cab46e048ed1756eb6669e0de7aac4850102ae
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-09-13 10:48:16 -0700

    Style.

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,33 +1,48 @@
 public interface Deque<T> {
 
-    /** Adds an item to the front of the deque. */
+    /**
+     * Adds an item to the front of the deque.
+     */
     void addFirst(T item);
 
-    /** Adds an item to the back of the deque. */
+    /**
+     * Adds an item to the back of the deque.
+     */
     void addLast(T item);
 
-    /** Returns true if empty; false otherwise. */
+    /**
+     * Returns true if empty; false otherwise.
+     */
     boolean isEmpty();
 
 
-    /** Returns the number of items in this deque. */
+    /**
+     * Returns the number of items in this deque.
+     */
     int size();
 
-    /** Prints each item of this deque line-by-line starting with the first
+    /**
+     * Prints each item of this deque line-by-line starting with the first
      * element.
-     * */
+     */
     void printDeque();
 
 
-    /** Removes and returns the first item of this deque. */
+    /**
+     * Removes and returns the first item of this deque.
+     */
     T removeFirst();
 
 
-    /** Removes and returns the last item of this deque. */
+    /**
+     * Removes and returns the last item of this deque.
+     */
     T removeLast();
 
 
-    /** Returns the ith item of this deque. */
+    /**
+     * Returns the ith item of this deque.
+     */
     T get(int i);
 }
 
