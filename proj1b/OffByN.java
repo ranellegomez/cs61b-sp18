9eedd609567b9e1a3b7476f8fb0147bb4caec170
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-09-13 10:43:51 -0700

    N -> _n

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,13 +1,13 @@
 public class OffByN implements CharacterComparator {
-    private int N;
+    private int _n;
 
     public OffByN(int n) {
-        this.N = n;
+        _n = n;
     }
 
     @Override
     /** Returns whether x and y are off by one. */
     public boolean equalChars(char x, char y) {
-        return Math.abs(x - y) == this.N;
+        return Math.abs(x - y) == _n;
     }
 }
