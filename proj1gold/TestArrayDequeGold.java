cf5386c154225259567afd5b702758256235bf53
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-09-14 18:01:11 -0700

    addFirst2

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -42,16 +42,61 @@ public class TestArrayDequeGold {
             int random = StdRandom.uniform(1000);
             actual.addLast(random);
             expected.addLast(random);
-            calls += "actual.addLast(" + random + ")" + "\n" + "expected"
-                    + ".addLast(" + random + ")" + "\n";
+            calls += "addLast(" + random + ")" + "\n";
         }
         for (int i = 0; i < 1000; i++) {
-            calls += "assertEquals(" + "expected.removeLast(), " +
-                    "actual.removeLast())" + "\n";
+            calls += "removeLast()" + "\n";
             assertEquals(calls, expected.removeLast(),
                          actual.removeLast());
         }
+    }
 
+    @Test
+    public void testRemoveLast2() {
+        StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
+        String calls = "\n";
+        for (int i = 0; i < 1000; i++) {
+            int random = StdRandom.uniform(1000);
+            int random2 = StdRandom.uniform(1000);
+
+            actual.addLast(random);
+            expected.addLast(random);
+
+            actual.addLast(random2);
+            expected.addLast(random2);
+
+            calls += "addLast(" + random + ")" + "\n";
+            calls += "addLast(" + random2 + ")" + "\n";
+            calls += "removeLast()" + "\n";
+
+            assertEquals(calls, expected.removeLast(),
+                         actual.removeLast());
+        }
+    }
+
+    @Test
+    public void testRemoveFirst2() {
+        StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
+        String calls = "\n";
+        for (int i = 0; i < 1000; i++) {
+            int random = StdRandom.uniform(1000);
+            int random2 = StdRandom.uniform(1000);
+
+            actual.addFirst(random);
+            expected.addFirst(random);
+
+            actual.addFirst(random2);
+            expected.addFirst(random2);
+
+            calls += "addFirst(" + random + ")" + "\n";
+            calls += "addFirst(" + random2 + ")" + "\n";
+            calls += "removeLast()" + "\n";
+
+            assertEquals(calls, expected.removeLast(),
+                         actual.removeLast());
+        }
     }
 
     @Test
