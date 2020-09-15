93d73e6cea7a87662cf4cfb4c0d5ad902b01e28e
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-09-14 18:13:54 -0700

    get

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,20 +1,20 @@
 import org.junit.Test;
 
-import java.util.Deque;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TestArrayDequeGold {
     @Test
     public void testAddFirst() {
         StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
-
+        String calls = "\n";
         for (int i = 0; i < 9999; i++) {
             int random = StdRandom.uniform(9999);
             actual.addFirst(random);
             expected.addFirst(random);
-            assertEquals(expected.removeFirst(), actual.removeFirst());
+            calls += "addFirst(" + random + ")" + "\n";
+            calls += "removeFirst()" + "\n";
+            assertEquals(calls, expected.removeFirst(), actual.removeFirst());
         }
     }
 
@@ -22,14 +22,17 @@ public class TestArrayDequeGold {
     public void testAddLast() {
         StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
+        String calls = "\n";
 
-        for (int i = 0; i < 1000; i++) {
-            int random = StdRandom.uniform(1000);
+        for (int i = 0; i < 9999; i++) {
+            int random = StdRandom.uniform(9999);
             actual.addLast(random);
             expected.addLast(random);
+            calls += "addLast(" + random + ")" + "\n";
         }
-        for (int i = 0; i < 1000; i++) {
-            assertEquals(actual.get(i), expected.get(i));
+        for (int i = 0; i < 9999; i++) {
+            calls += "get(" + i + ")" + "\n";
+            assertEquals(calls, actual.get(i), expected.get(i));
         }
     }
 
@@ -38,13 +41,13 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
         String calls = "\n";
-        for (int i = 0; i < 1000; i++) {
-            int random = StdRandom.uniform(1000);
+        for (int i = 0; i < 9999; i++) {
+            int random = StdRandom.uniform(9999);
             actual.addLast(random);
             expected.addLast(random);
             calls += "addLast(" + random + ")" + "\n";
         }
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 9999; i++) {
             calls += "removeLast()" + "\n";
             assertEquals(calls, expected.removeLast(),
                          actual.removeLast());
@@ -56,9 +59,9 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
         String calls = "\n";
-        for (int i = 0; i < 1000; i++) {
-            int random = StdRandom.uniform(1000);
-            int random2 = StdRandom.uniform(1000);
+        for (int i = 0; i < 9999; i++) {
+            int random = StdRandom.uniform(9999);
+            int random2 = StdRandom.uniform(9999);
 
             actual.addLast(random);
             expected.addLast(random);
@@ -80,9 +83,9 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
         String calls = "\n";
-        for (int i = 0; i < 1000; i++) {
-            int random = StdRandom.uniform(1000);
-            int random2 = StdRandom.uniform(1000);
+        for (int i = 0; i < 9999; i++) {
+            int random = StdRandom.uniform(9999);
+            int random2 = StdRandom.uniform(9999);
 
             actual.addFirst(random);
             expected.addFirst(random);
@@ -100,20 +103,42 @@ public class TestArrayDequeGold {
     }
 
     @Test
+    public void testRemoveFirst3() {
+        StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
+        String calls = "\n";
+        for (int i = 0; i < 9999; i++) {
+            int random = StdRandom.uniform(9999);
+            int random2 = StdRandom.uniform(9999);
+
+            actual.addFirst(random);
+            expected.addFirst(random);
+
+            actual.addFirst(random2);
+            expected.addFirst(random2);
+
+            calls += "addFirst(" + random + ")" + "\n";
+            calls += "addFirst(" + random2 + ")" + "\n";
+            calls += "removeFirst()" + "\n";
+
+            assertEquals(calls, expected.removeFirst(),
+                         actual.removeFirst());
+        }
+    }
+
+    @Test
     public void testRemoveFirst() {
         StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
         String calls = "\n";
-        for (int i = 0; i < 1000; i++) {
-            int random = StdRandom.uniform(1000);
+        for (int i = 0; i < 9999; i++) {
+            int random = StdRandom.uniform(9999);
             actual.addFirst(random);
             expected.addFirst(random);
-            calls += "actual.First(" + random + ")" + "\n" + "expected"
-                    + ".addFirst(" + random + ")" + "\n";
+            calls += "addFirst(" + random + ")" + "\n";
         }
-        for (int i = 0; i < 1000; i++) {
-            calls += "assertEquals(" + "expected.removeFirst(), " +
-                    "actual.removeFirst())" + "\n";
+        for (int i = 0; i < 9999; i++) {
+            calls += "removeFirst(), " + "\n";
             assertEquals(calls, expected.removeFirst(),
                          actual.removeFirst());
         }
